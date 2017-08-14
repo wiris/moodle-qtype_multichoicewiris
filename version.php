415fd7e6187c754fd76cff262067d345395f777a
Manuel <manuel@wiris.com>
2017-08-14 22:59:05 +0200

    2017081400 development version

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017081000;
-$plugin->release = '3.55.0.1025';
+$plugin->version = 2017081400;
+
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'qtype_multichoicewiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2017081000
+     'qtype_wq' => 2017081400
 );

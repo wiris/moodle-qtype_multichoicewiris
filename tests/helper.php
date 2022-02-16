f51112bf66e9dcfd639c0b0b25a5bfb2b4ce9a04
Marc Vila Tornamorell <marc.vila@wiris.com>
2022-02-16 11:26:33 +0100

    fix: moodle code checker fix

--- tests/helper.php
+++ tests/helper.php
@@ -29,11 +29,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
-// global $CFG;
-// $CFG->behat_increasetimeout = 3;
-
 class qtype_multichoicewiris_test_helper extends question_test_helper {
     public function get_test_questions() {
         return array('four_of_five_science');
